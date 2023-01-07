002adb917ea646aca7896bba2cd89c7edae8f308
adelmnv <99648706+adelmnv@users.noreply.github.com>
2023-01-07 15:05:27 +0600

    homework for day 3

--- AspNet/Models/Account.cs
+++ AspNet/Models/Account.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace AspNet
+namespace AspNet.Models
 {
-    public class Class1
+    public class Account
     {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
