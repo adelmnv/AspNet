a313815b9aa523d35eb2a6915c3e58eb662516c7
adelmnv <99648706+adelmnv@users.noreply.github.com>
2023-01-07 14:43:28 +0600

    Homework for Day2

--- AspNet/Controllers/HomeController.cs
+++ AspNet/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 
 namespace AspNet.Controllers
 {
+    /*Контроллер Home представляет папку Home которая расположена внутри папки Views. Контроллер определяет нужное представление которое запросил пользователь*/
+    /*Контроллер Home связан с каждым .cshtml файлом с помощью методов чьи названия соответсвуют названиям .cshtml файлам*/
+    /*return View() - возвращает представление с таким же именем как у метода из которого он был вызван*/
     public class HomeController : Controller
     {
         public ActionResult Index()
