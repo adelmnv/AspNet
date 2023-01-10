9be27e092caafae75280c73310b27ccfdbfa0026
adelmnv <99648706+adelmnv@users.noreply.github.com>
2023-01-10 13:13:02 +0600

    Homework for Day4

--- AspNet/Controllers/RegisterController.cs
+++ AspNet/Controllers/RegisterController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspNet.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,19 @@ namespace AspNet.Controllers
 {
     public class RegisterController : Controller
     {
-        // GET: Register
+        [HttpGet]
         public ActionResult Index()
         {
+            ViewBag.Date = DateTime.Now;
             return View();
         }
+
+        [HttpPost]
+        public string Index(Account account)
+        {
+            return "Thank you for registration " + account.FirstName + " "+ account.LastName + " !";
+        }
+
+       
     }
 }
