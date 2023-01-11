9f911076fe959ebbf5bb824a4d35196f8f522fe8
adelmnv <99648706+adelmnv@users.noreply.github.com>
2023-01-11 20:22:06 +0600

    Day5

---
+++ AspNet/Controllers/InfoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNet.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        public string Index()
+        {
+            HttpContext.Response.Write("<h1>Welcome to HttpContext</h1>");
+
+            string browser = HttpContext.Request.Browser.Browser;
+            string user_agent = HttpContext.Request.UserAgent;
+            string url = HttpContext.Request.RawUrl;
+            string ip = HttpContext.Request.UserHostAddress;
+            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+
+            return "<p>Browser: " + browser + "</p><p>User Agent: " + user_agent + "</p><p>Url: " + url + "</p><p>IP: " + ip + "</p><p>Referrer: " + referrer + "</p>";
+        }
+    }
+}
