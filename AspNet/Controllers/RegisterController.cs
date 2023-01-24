d66c004b9ab8f7d0e066004195f400c171923a73
adelmnv <99648706+adelmnv@users.noreply.github.com>
2023-01-24 13:07:24 +0600

    Homework for Day8

--- AspNet/Controllers/RegisterController.cs
+++ AspNet/Controllers/RegisterController.cs
@@ -18,16 +18,15 @@ namespace AspNet.Controllers
         public ActionResult SignUp()
         {
             ViewBag.Date = DateTime.Now;
-            //ViewBag.Id = db.Accounts.Count() == 0 ? 1 : db.Accounts.Count()+1;
             return View();
         }
 
         [HttpPost]
-        public string SignUp(Account account)
+        public ActionResult SignUp(Account account)
         {
             db.Accounts.Add(account);
             db.SaveChanges();
-            return "Thank you for registration " + account.FirstName + " " + account.LastName + " !";
+            return RedirectToAction("Index"); 
         }
 
         public ActionResult LogIn()
@@ -41,8 +40,62 @@ namespace AspNet.Controllers
             
             Account account = db.Accounts.Where(x=> x.Email == Email).FirstOrDefault();
             if(account == null) { return "Account is not found"; }
-            return $"Hello {account.LastName} {account.FirstName}!";
+            return $"<p>Hello {account.LastName} {account.FirstName}!<div><a href=\"/Register/ViewAll\">View All</a> if you want to see all accounts</div>";
         }
+
+        public ActionResult ViewAll()
+        {
+            if (db.Accounts.Count() == 0)
+                return RedirectToAction("Index");
+            return View(db.Accounts.ToList());
+            
+        }
+
+        public ActionResult Details(int? id)
+        {
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+                return HttpNotFound();
+            return View(account);
+        }
+        
+        public ActionResult Edit(int? id)
+        {
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+                return HttpNotFound();
+            return View(account);
+        }
+
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,Password,Email,Gender,CreatedDate")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(account).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ViewAll");
+            }
+            return View(account);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+                return HttpNotFound();
+            return View(account);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult ConfirmedDelete(int? id)
+        {
+            Account account = db.Accounts.Find(id);
+            db.Accounts.Remove(account);
+            db.SaveChanges();
+            return RedirectToAction("ViewAll");
+        }
+
 
     }
 }
