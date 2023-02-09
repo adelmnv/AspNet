cea3142cf69a0fdbfacc81ef205ac3c8caa59a75
adelmnv <99648706+adelmnv@users.noreply.github.com>
2023-02-09 13:27:31 +0600

    Homework for Day13

---
+++ AspNet/Migrations/202302090725062_Second.cs
@@ -0,0 +1,26 @@
+﻿namespace AspNet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Second : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Accounts", "FirstName", c => c.String(nullable: false, maxLength: 20));
+            AlterColumn("dbo.Accounts", "LastName", c => c.String(nullable: false, maxLength: 20));
+            AlterColumn("dbo.Accounts", "Password", c => c.String(nullable: false, maxLength: 15));
+            AlterColumn("dbo.Accounts", "Email", c => c.String(nullable: false));
+            AlterColumn("dbo.Accounts", "Gender", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Accounts", "Gender", c => c.String());
+            AlterColumn("dbo.Accounts", "Email", c => c.String());
+            AlterColumn("dbo.Accounts", "Password", c => c.String());
+            AlterColumn("dbo.Accounts", "LastName", c => c.String());
+            AlterColumn("dbo.Accounts", "FirstName", c => c.String());
+        }
+    }
+}
--- AspNet/Models/Account.cs
+++ AspNet/Models/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,25 @@ namespace AspNet.Models
     public class Account
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "This is required field")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Min = 2, Max=50 simbols for field")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "This is required field")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Min = 2, Max=20 simbols for field")]
         public string LastName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This is required field")]
+        [StringLength(15, MinimumLength = 6, ErrorMessage = "Min = 6, Max=15 simbols for field")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "This is required field")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Incorrec email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "This is required field")]
+        [RegularExpression(@"^man|woman",ErrorMessage = "You can write only man or woman")]
         public string Gender { get; set; }
         public DateTime CreatedDate { get; set; }
     }
